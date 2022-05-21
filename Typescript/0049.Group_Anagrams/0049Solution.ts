4f0948e8588e079098e00a1d76ea25ef27f85e05
syueer <syue531@hotmail.com>
2022-05-21 22:56:23 +1000

    change to in operator

--- Typescript/0049.Group_Anagrams/0049Solution.ts
+++ Typescript/0049.Group_Anagrams/0049Solution.ts
@@ -2,11 +2,10 @@ function groupAnagrams(strs: string[]): string[][] {
   let obj: Record<string, string[]> = {};
   for (let i = 0; i < strs.length; i++) {
     let key: string = strs[i].split('').sort().join('');
-    console.log(typeof key);
-    if (obj === {} || !obj[key]) {
-      obj[key] = [strs[i]];
-    } else {
+    if (key in obj) {
       obj[key].push(strs[i]);
+    } else {
+      obj[key] = [strs[i]];
     }
   }
   let result: string[][] = Object.values(obj);
